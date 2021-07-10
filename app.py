d23c2e748c50a046f5b946eeb90537653aadcd97
Jeff Lai <jefflai333@gmail.com>
2021-07-10 15:44:26 -0400

    Formats api request to json

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ import message_querer
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/table", methods=["GET"])
 def show_stats():
     count = message_querer.query_aggregate_data_from_db()
-    return str(count)
+    return {
+        "count": count[0]
+    }
