57aee91a891abe9dc14315d60a630f2f1f6698d5
Jeff Lai <jefflai333@gmail.com>
2021-07-19 18:21:39 -0400

    Adds error checking for connection to DB

--- message_indexer.py
+++ message_indexer.py
@@ -3,22 +3,26 @@ from datetime import datetime
 
 
 def message_indexer(jsonData):
-    conn = psycopg2.connect(dbname="test", user="postgres",
+    try:
+        conn = psycopg2.connect(dbname="test", user="postgres",
                             password="password", host="localhost", port="5433")
-    cur = conn.cursor()
-    cur.execute("DROP TABLE messages")
-    cur.execute("CREATE TABLE IF NOT EXISTS messages (id SERIAL, sender VARCHAR(255) NOT NULL, date TIMESTAMP NOT NULL, message VARCHAR(4095), type VARCHAR(63) NOT NULL, title VARCHAR(255), PRIMARY KEY (id));")
-    messages = jsonData["messages"]
-    title = jsonData["title"]
-    for message in messages:
-        sender = message["sender_name"]
-        dt = datetime.fromtimestamp(message["timestamp_ms"]/1000)
-        msg = None
-        if "content" in message:
-            msg = message["content"]
-        type = message["type"]
-        cur.execute(
-            "INSERT INTO messages (sender, date, message, type, title) VALUES (%s, %s, %s, %s, %s)", (sender, dt, msg, type, title))
-    conn.commit()
-    cur.close()
-    conn.close()
+    except Exception as err:
+        print("error msg:", err)
+        conn = None
+    if conn != None:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS messages (id SERIAL, sender VARCHAR(255) NOT NULL, date TIMESTAMP NOT NULL, message VARCHAR(4095), type VARCHAR(63) NOT NULL, title VARCHAR(255), PRIMARY KEY (id));")
+        messages = jsonData["messages"]
+        title = jsonData["title"]
+        for message in messages:
+            sender = message["sender_name"]
+            dt = datetime.fromtimestamp(message["timestamp_ms"]/1000)
+            msg = None
+            if "content" in message:
+                msg = message["content"]
+            type = message["type"]
+            cur.execute(
+                "INSERT INTO messages (sender, date, message, type, title) VALUES (%s, %s, %s, %s, %s)", (sender, dt, msg, type, title))
+        conn.commit()
+        cur.close()
+        conn.close()
--- message_querer.py
+++ message_querer.py
@@ -2,15 +2,21 @@ import psycopg2
 
 
 def query_aggregate_data_from_db():
-    conn = psycopg2.connect(dbname="test", user="postgres",
+    try:
+        conn = psycopg2.connect(dbname="test", user="postgres",
                             password="password", host="localhost", port="5433")
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT COUNT(*), COUNT(CASE WHEN title=sender then 1 ELSE NULL END), COUNT(CASE WHEN title!=sender then 1 ELSE NULL END) from messages;")
-    total_rows = cur.fetchone()
-    cur.execute("SELECT title from messages GROUP BY title;")
-    total_rows = total_rows + cur.fetchone()
-    conn.commit()
-    cur.close()
-    conn.close()
-    return total_rows
+    except Exception as err:
+        print("error msg:", err)
+        conn = None
+    if conn != None:
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT COUNT(*), COUNT(CASE WHEN title=sender then 1 ELSE NULL END), COUNT(CASE WHEN title!=sender then 1 ELSE NULL END) from messages;")
+        total_rows = cur.fetchone()
+        cur.execute("SELECT title from messages GROUP BY title;")
+        total_rows = total_rows + cur.fetchone()
+        conn.commit()
+        cur.close()
+        conn.close()
+        return total_rows
+    return None
